591eba8257344ae20c87a316c83e91fe2f8cb896
Shubham <shubhamp1251@gmail.com>
2026-09-17 14:13:36 +0530

    Refactor product and user reducers for improved code organization and readability; update store configuration to maintain consistency across reducers.

--- backend/middleware/catchAsyncErrors.js
+++ backend/middleware/catchAsyncErrors.js
@@ -1,3 +1,6 @@
 module.exports = Func => (req,res,next) => {
-    Promise.resolve(Func(req,res,next)).catch(next);
+    Promise.resolve(Func(req, res, next)).catch(err => {
+        console.log("Error caught in catchAsyncErrors middleware:", err);
+        next(err);
+    });
 }
